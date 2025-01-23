4d29e081a05fa2d1212e8abea6abbc1a6b6ce88f
Yen <yuxxxlouyen@gmail.com>
2025-01-23 14:44:45 +0800

    2nd commit

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -5,15 +5,21 @@ function Pagination({pageInfo, btnChangePage}) {
             <nav>
                 <ul className={`pagination ${pageInfo.total_pages <= 1 && 'd-none'}`}>
                     <li className={`page-item ${!pageInfo.has_pre && 'disabled'}`}>
-                        <a className="page-link" href="#" onClick={() => btnChangePage(pageInfo.current_page - 1)}>上一頁</a>
+                        <a className="page-link" href="#" onClick={(e) => {
+                            e.preventDefault();
+                            btnChangePage(pageInfo.current_page - 1)}}>上一頁</a>
                     </li>
                     {Array.from({ length: pageInfo.total_pages }).map((item, index) => (
                         <li className={`page-item ${pageInfo.current_page === index + 1 && 'active'}`} key={index}>
-                            <a className="page-link" href="#" onClick={() => btnChangePage(index + 1)}>{index + 1}</a>
+                            <a className="page-link" href="#" onClick={(e) => {
+                                e.preventDefault();
+                                btnChangePage(index + 1)}}>{index + 1}</a>
                         </li>))}
 
                     <li className={`page-item ${!pageInfo.has_next && 'disabled'}`}>
-                        <a className="page-link" href="#" onClick={() => btnChangePage(pageInfo.current_page + 1)}>下一頁</a>
+                        <a className="page-link" href="#" onClick={(e) => {
+                            e.preventDefault();
+                            btnChangePage(pageInfo.current_page + 1)}}>下一頁</a>
                     </li>
                 </ul>
             </nav>
